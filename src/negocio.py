a6ba43a1249731edd0c95ba2fd0d4c9882b623fe
Claudio Berrondo <claudio.berrondo@gmail.com>
2025-09-28 02:08:04 -0300

    renames

--- src/negocio.py
+++ src/negocio.py
@@ -5,7 +5,7 @@ def _estrategia_eh_comprar(j, p):
     return j.estrategia(j, p)
 
 
-def _comprar(j, p):
+def _tentar_comprar(j, p):
     if jogador.tem_saldo(j, p.preco) and _estrategia_eh_comprar(j, p):
         j2 = jogador.debitar(j, p.preco)
         p2 = propriedade.apropriar(p, j2)
@@ -13,7 +13,7 @@ def _comprar(j, p):
     return j, p
 
 
-def _alugar(j, p):
+def _tentar_alugar(j, p):
     if jogador.tem_saldo(j, p.aluguel):
         j2 = jogador.debitar(j, p.aluguel)
         jp2 = jogador.creditar(p.proprietario, p.aluguel)
@@ -24,6 +24,6 @@ def _alugar(j, p):
 
 def comprar_ou_alugar(j, p):
     if not propriedade.tem_proprietario(p):
-        return _comprar(j, p)
+        return _tentar_comprar(j, p)
     else:
-        return _alugar(j, p)
+        return _tentar_alugar(j, p)
