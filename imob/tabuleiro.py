2630a79a5cdd8f4e98eb1861238977e2410ddf9f
Claudio Berrondo <claudio.berrondo@gmail.com>
2025-09-30 02:13:02 -0300

    bonus na volta do tabuleiro

--- imob/tabuleiro.py
+++ imob/tabuleiro.py
@@ -1,5 +1,9 @@
 from pyrsistent import m, v
 
+from imob import jogador
+
+BONUS = 100
+
 
 def criar_tabuleiro(propriedades, jogadores):
     return m(
@@ -9,10 +13,19 @@ def criar_tabuleiro(propriedades, jogadores):
     )
 
 
+def extensao(self):
+    return len(self.propriedades)
+
+
 def posicao_do_jogador(self, j):
     return self.posicoes[j.nome]
 
 
 def mover_jogador(self, j, passos):
-    nova_posicao = posicao_do_jogador(self, j) + passos
+    posicao = posicao_do_jogador(self, j)
+    nova_volta, nova_posicao = divmod(posicao + passos, extensao(self))
+    if nova_volta:  # bonifica
+        j2 = jogador.creditar(self.jogadores[j.nome], BONUS)
+        js = self.jogadores.set(j.nome, j2)
+        self = self.set('jogadores', js)
     return self.set('posicoes', self.posicoes.set(j.nome, nova_posicao))
--- tests/test_tabuleiro.py
+++ tests/test_tabuleiro.py
@@ -1,7 +1,7 @@
 from imob.estrategia import estrategias, impulsivo
 from imob.jogador import criar_jogador
 from imob.propriedade import criar_propriedade
-from imob.tabuleiro import criar_tabuleiro, mover_jogador, posicao_do_jogador
+from imob.tabuleiro import criar_tabuleiro, extensao, mover_jogador, posicao_do_jogador
 
 jogadores = [j for j in [criar_jogador(e, 300) for e in estrategias]]
 
@@ -11,6 +11,7 @@ propriedades = [criar_propriedade(preco=0, aluguel=0)] * 20
 def test_criar_tabuleiro():
     t = criar_tabuleiro(propriedades, jogadores)
 
+    assert extensao(t) == 20
     assert len(t.propriedades) == 20
     assert len(t.jogadores) == 4
     assert len(t.posicoes) == 4
@@ -18,10 +19,23 @@ def test_criar_tabuleiro():
 
 def test_move_jogador_um_passo():
     j1 = criar_jogador(impulsivo, 300)
-    t1 = criar_tabuleiro(propriedades=[], jogadores=[j1])
+    t1 = criar_tabuleiro(propriedades=propriedades, jogadores=[j1])
 
     assert posicao_do_jogador(t1, j1) == -1
 
     t2 = mover_jogador(t1, j1, 1)
 
     assert posicao_do_jogador(t2, j1) == 0
+
+
+def test_jogador_volta_ao_inicio_do_tabuleiro():
+    j1 = criar_jogador(impulsivo, 300)
+    t1 = criar_tabuleiro(propriedades=propriedades, jogadores=[j1])
+
+    assert extensao(t1) == 20
+    assert posicao_do_jogador(t1, j1) == -1
+
+    t2 = mover_jogador(t1, j1, 25)
+
+    assert posicao_do_jogador(t2, j1) == 4
+    assert t2.jogadores[j1.nome].saldo == 400
